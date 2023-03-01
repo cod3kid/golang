6151a052df30240309a3db024f3119032514609b
cod3kid <djsufail@gmail.com>
2023-03-01 14:57:31 +0530

    Read a file

---
+++ misc/read-a-file.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+    "fmt"
+	"io/ioutil"
+)
+
+func main(){
+
+	file, err := os.Open("dummy.txt")
+	if err != nil{
+		return
+	}
+
+	defer file.Close()
+
+	// Reading File Stats
+	stats, err := file.Stat()
+	if err != nil{
+		return 
+	}
+
+	fmt.Printf("%+v\n",stats)
+
+	contents, err := ioutil.ReadFile("dummy.txt")
+
+	if err!= nil{
+		return
+	}
+
+	fmt.Println("\n",string(contents))
+
+
+
+}
