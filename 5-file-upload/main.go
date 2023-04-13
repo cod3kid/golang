d43a43e789c6b187d8cc20f6de478b4900ea5238
cod3kid <djsufail@gmail.com>
2023-04-13 19:53:53 +0530

    HTML Rendering

--- 5-file-upload/main.go
+++ 5-file-upload/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"log"
+	"fmt"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,13 +15,18 @@ func main() {
 	//router setup
 	router := gin.Default()
 	router.Static("/assets", "./assets")
-	router.LoadHTMLGlob("templates/*")
 
 	//routes
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
-		})
+	router.MaxMultipartMemory = 8 << 20  // 8 MiB
+	router.POST("/upload", func(c *gin.Context) {
+		// single file
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		// Upload the file to specific dst.
+		c.SaveUploadedFile(file, "uploads/"+file.Filename)
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
 	})
 
 	// Listening to port..  by default it listens to 8080 router.Run()
