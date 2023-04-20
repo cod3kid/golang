c58d2fb2de3700ea1d3ac0f27b3dc5874e44ada4
cod3kid <djsufail@gmail.com>
2023-04-20 19:33:59 +0530

    Get the body on the API resolver

--- 10-authentication/main.go
+++ 10-authentication/main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"github.com/cod3kid/golang/10-authentication/models"
 	"net/http"
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func HelloWorld(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"data": "Hello World"})
+}
+
+func SignUp(c *gin.Context) {
+	var body struct {
+		Name string
+		Email string
+		Password string
+	}
+
+	if c.Bind(&body) == nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No request body found"})
+	}
+
+	fmt.Printf("%+v",body)
 	c.JSON(http.StatusOK, gin.H{"data": "Hello World"})
 }
 
@@ -16,7 +32,7 @@ func main() {
 
 	models.ConnectDatabase()
 	router.GET("/", HelloWorld)
-	
+	router.POST("/signup", SignUp)	
 
 	router.Run()
 }
