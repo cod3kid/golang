36dce0f6759b72d14d4a58b6f578a139105003c4
cod3kid <djsufail@gmail.com>
2023-04-22 20:00:05 +0530

    Fetch existing user

--- 10-authentication/main.go
+++ 10-authentication/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/cod3kid/golang/10-authentication/models"
 	"golang.org/x/crypto/bcrypt"
+	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
@@ -30,9 +31,36 @@ func SignUp(c *gin.Context) {
 	}
 
 	user := models.User{Name: body.Name, Email: body.Email, Password: string(hashedPassword)}
-	models.DB.Create(&user)
+	result := models.DB.Create(&user)
 
+	if result.Error != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create a user"})
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{"data": user, "message":"User created"})
+}
+
+func Login(c *gin.Context) {
+	var body struct {
+		Email string
+		Password string
+	}
+
+	if c.Bind(&body) != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No request body found"})
+		return
+	}
+
+	var user models.User
+	models.DB.Where("email = ?", body.Email).First(&user)
+
+	if user.ID == 0{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
+		return
+	}
+
+	fmt.Printf("%+v",user)
+
 }
 
 
@@ -41,7 +69,9 @@ func main() {
 
 	models.ConnectDatabase()
 	router.GET("/", HelloWorld)
-	router.POST("/signup", SignUp)	
+	router.POST("/signup", SignUp)
+	router.POST("/login", Login)	
+	
 
 	router.Run()
 }
