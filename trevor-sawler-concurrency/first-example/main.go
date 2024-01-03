b3420b1b6f3387a00077003f6848e9506ac27b7d
cod3kid <djsufail@gmail.com>
2024-01-03 08:12:34 +0530

    go routine using WaitGroups

--- trevor-sawler-concurrency/first-example/main.go
+++ trevor-sawler-concurrency/first-example/main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 "fmt"
-"time"
+"sync"
 )
 
 
-func printSomething(s string){
+func printSomething(s string, wg *sync.WaitGroup){
+	defer wg.Done()
+
 	fmt.Println(s)
 }
 func main(){
-	go printSomething("First")
+	var wg sync.WaitGroup
 
-	time.Sleep(1*time.Second)
-	printSomething("Second")
+	wg.Add(9)
+	words := []string{
+		"alpha",
+		"beta",
+		"gamma",
+		"omega",
+		"delta",
+		"epsilon",
+		"zeta",
+		"eta",
+		"theta",
+	}	
+
+	for i,x := range words{
+		go printSomething(fmt.Sprintf("%d %s",i,x),&wg)
+	}
+
+	wg.Wait()
+
+	wg.Add(1)
+	printSomething("Second",&wg)
 }
