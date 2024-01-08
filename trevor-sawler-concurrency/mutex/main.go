6df14ba48472d4d18b69c312ce8a9b0785dcb402
cod3kid <djsufail@gmail.com>
2024-01-08 09:34:57 +0530

    Mutex - 1

--- trevor-sawler-concurrency/mutex/main.go
+++ trevor-sawler-concurrency/mutex/main.go
@@ -8,17 +8,21 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string){
+func updateMessage(s string, m *sync.Mutex){
 defer wg.Done()
+
+m.Lock()
 msg = s
+m.Unlock()
 }
 
 func main(){
 msg = "Hello World"
+var mutex sync.Mutex
 
 wg.Add(2)
-go updateMessage("Hello, Universe")
-go updateMessage("Hello, Cosmos")
+go updateMessage("Hello, Universe", &mutex)
+go updateMessage("Hello, Cosmos",&mutex)
 wg.Wait()
 
 fmt.Println(msg)
