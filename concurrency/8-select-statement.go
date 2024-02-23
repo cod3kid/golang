897cf5c729cd7dc8ab925338d14435df92d6e18f
cod3kid <djsufail@gmail.com>
2024-02-24 01:55:53 +0530

    Select statement - default

--- concurrency/8-select-statement.go
+++ concurrency/8-select-statement.go
@@ -13,7 +13,7 @@ func main() {
 		for i := 0; i < 5; i++ {
 			channel1 <- "I'll print every 100ms"
 			time.Sleep(time.Millisecond * 100)
-			
+
 		}
 	}()
 
@@ -21,18 +21,19 @@ func main() {
 		for i := 0; i < 5; i++ {
 			channel2 <- "I'll print every 1s"
 			time.Sleep(time.Second * 1)
-			
+
 		}
 	}()
 
 	for i := 0; i < 5; i++ {
-		
-		select{
-		case  message1 := <-channel1:
-			  fmt.Println(message1)
-		case  message2 := <-channel2:
-			  fmt.Println(message2)
+
+		select {
+		case message1 := <-channel1:
+			fmt.Println(message1)
+		case message2 := <-channel2:
+			fmt.Println(message2)
+		default:
+			fmt.Println("No channel is ready")
 		}
-		
 	}
 }
