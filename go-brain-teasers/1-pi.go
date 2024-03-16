0a119b628d7a3c1379f74b54a289b0535bb971d5
cod3kid <djsufail@gmail.com>
2024-03-17 02:21:53 +0530

    Go Brain Teasers: Pi part 2

--- go-brain-teasers/1-pi.go
+++ go-brain-teasers/1-pi.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
-	// Go accepts Unicode characters as variable names
-	var π = 22 / 7.0
-	fmt.Println(π)
+func main() {
+	        // Go accepts Unicode characters as variable names
+			// The Go type system will not allow dividing (or any other mathematical operation) between an integer (22) and a float (7.0).
+			// But if it's 22/7.0, it'll allow
+        var a, b = 22, 7.0
+        var π = a / b
+        fmt.Println(π)
 }
