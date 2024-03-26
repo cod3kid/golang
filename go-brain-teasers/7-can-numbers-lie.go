8a0091264a086339fd8d5f446a441a075da368cf
cod3kid <djsufail@gmail.com>
2024-03-27 02:26:46 +0530

    Go Brain Teasers: Can numbers lie? - NaN

--- go-brain-teasers/7-can-numbers-lie.go
+++ go-brain-teasers/7-can-numbers-lie.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 	//  we sacrifice accuracy for speed. It's the same JavaScript, Python etc...
 	n := 1.1
 	fmt.Println(n * n)
+
+	//  The NaN value does not equal any number, including itself. It's the same in JavaScript
+	fmt.Println(math.NaN() == math.NaN())
 }
