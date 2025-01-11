d25f128c1e0187268641e14cc855410961d09a5a
cod3kid <djsufail@gmail.com>
2025-01-11 07:08:29 +0530

    Dining Philosophers Problem - Part 2

--- trevor-sawler-concurrency/dining-philosophers-problem/main.go
+++ trevor-sawler-concurrency/dining-philosophers-problem/main.go
@@ -2,61 +2,157 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 	"sync"
+	"time"
 )
 
-type Philospher struct {
+// The Dining Philosophers problem is well known in computer science circles.
+// Five philosophers, numbered from 0 through 4, live in a house where the
+// table is laid for them; each philosopher has their own place at the table.
+// Their only difficulty – besides those of philosophy – is that the dish
+// served is a very difficult kind of spaghetti which has to be eaten with
+// two forks. There are two forks next to each plate, so that presents no
+// difficulty. As a consequence, however, this means that no two neighbours
+// may be eating simultaneously, since there are five philosophers and five forks.
+//
+// This is a simple implementation of Dijkstra's solution to the "Dining
+// Philosophers" dilemma.
+
+// Philosopher is a struct which stores information about a philosopher.
+type Philosopher struct {
 	name      string
 	rightFork int
 	leftFork  int
 }
 
-var philosophers = []Philospher{
-	{name:"Plato",leftFork:4,rightFork:0},
-	{name:"Socrates",leftFork:0,rightFork:1},
-	{name:"Aristotle",leftFork:1,rightFork:2},
-	{name:"Pascal",leftFork:2,rightFork:3},
-	{name:"Locke",leftFork:3,rightFork:4},
+// philosophers is list of all philosophers.
+var philosophers = []Philosopher{
+	{name: "Plato", leftFork: 4, rightFork: 0},
+	{name: "Socrates", leftFork: 0, rightFork: 1},
+	{name: "Aristotle", leftFork: 1, rightFork: 2},
+	{name: "Pascal", leftFork: 2, rightFork: 3},
+	{name: "Locke", leftFork: 3, rightFork: 4},
 }
 
-var hunger = 3
-var eatTime = 1 * time.Second
-var thinkTime = 3 * time.Second
-var sleepTime = 1* time.Second
+// Define a few variables.
+var hunger = 3                  // how many times a philosopher eats
+var eatTime = 1 * time.Second   // how long it takes to eatTime
+var thinkTime = 3 * time.Second // how long a philosopher thinks
+var sleepTime = 1 * time.Second // how long to wait when printing things out
 
-func dine(){
-wg := &sync.WaitGroup{}
-wg.Add(len(philosophers))
-
-seated:=&sync.WaitGroup{}
-seated.Add(len(philosophers))
-
-var forks = make(map[int]*sync.Mutex)
-
-for i:=0;i<len(philosophers);i++{
-	forks[i]=&sync.Mutex{}
-}
-
-for i:=0;i<len(philosophers);i++{
-	go diningProblem(philosophers[i],wg,forks,seated)
-}
-
-wg.Wait()
-}
-
-func diningProblem(philosopher Philospher, wg *sync.WaitGroup,forks map[int]*sync.Mutex, seated *sync.WaitGroup){
-defer wg.Done()
-}
+// *** added this
+var orderMutex sync.Mutex  // a mutex for the slice orderFinished; part of challenge!
+var orderFinished []string // the order in which philosophers finish dining and leave; part of challenge!
 
 func main() {
+	// print out a welcome message
 	fmt.Println("Dining Philosophers Problem")
 	fmt.Println("---------------------------")
 	fmt.Println("The table is empty.")
 
+	// *** added this
+	time.Sleep(sleepTime)
+
+	// start the meal
 	dine()
 
+	// print out finished message
 	fmt.Println("The table is empty.")
+
+	// *** added this
+	time.Sleep(sleepTime)
+	fmt.Printf("Order finished: %s.\n", strings.Join(orderFinished, ", "))
 
 }
 
+func dine() {
+	eatTime = 0 * time.Second
+	sleepTime = 0 * time.Second
+	thinkTime = 0 * time.Second
+
+	// wg is the WaitGroup that keeps track of how many philosophers are still at the table. When
+	// it reaches zero, everyone is finished eating and has left. We add 5 (the number of philosophers) to this
+	// wait group.
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
+
+	// We want everyone to be seated before they start eating, so create a WaitGroup for that, and set it to 5.
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	// forks is a map of all 5 forks. Forks are assigned using the fields leftFork and rightFork in the Philosopher
+	// type. Each fork, then, can be found using the index (an integer), and each fork has a unique mutex.
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// Start the meal by iterating through our slice of Philosophers.
+	for i := 0; i < len(philosophers); i++ {
+		// fire off a goroutine for the current philosopher
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+
+	// Wait for the philosophers to finish. This blocks until the wait group is 0.
+	wg.Wait()
+}
+
+// diningProblem is the function fired off as a goroutine for each of our philosophers. It takes one
+// philosopher, our WaitGroup to determine when everyone is done, a map containing the mutexes for every
+// fork on the table, and a WaitGroup used to pause execution of every instance of this goroutine
+// until everyone is seated at the table.
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
+
+	// seat the philosopher at the table
+	fmt.Printf("%s is seated at the table.\n", philosopher.name)
+
+	// Decrement the seated WaitGroup by one.
+	seated.Done()
+
+	// Wait until everyone is seated.
+	seated.Wait()
+
+	// Have this philosopher eatTime and thinkTime "hunger" times (3).
+	for i := hunger; i > 0; i-- {
+		// Get a lock on the left and right forks. We have to choose the lower numbered fork first in order
+		// to avoid a logical race condition, which is not detected by the -race flag in tests; if we don't do this,
+		// we have the potential for a deadlock, since two philosophers will wait endlessly for the same fork.
+		// Note that the goroutine will block (pause) until it gets a lock on both the right and left forks.
+		if philosopher.leftFork > philosopher.rightFork {
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+		} else {
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+		}
+
+		// By the time we get to this line, the philosopher has a lock (mutex) on both forks.
+		fmt.Printf("\t%s has both forks and is eating.\n", philosopher.name)
+		time.Sleep(eatTime)
+
+		// The philosopher starts to think, but does not drop the forks yet.
+		fmt.Printf("\t%s is thinking.\n", philosopher.name)
+		time.Sleep(thinkTime)
+
+		// Unlock the mutexes for both forks.
+		forks[philosopher.leftFork].Unlock()
+		forks[philosopher.rightFork].Unlock()
+
+		fmt.Printf("\t%s put down the forks.\n", philosopher.name)
+	}
+
+	// The philosopher has finished eating, so print out a message.
+	fmt.Println(philosopher.name, "is satisified.")
+	fmt.Println(philosopher.name, "left the table.")
+
+	// *** added this
+	orderMutex.Lock()
+	orderFinished = append(orderFinished, philosopher.name)
+	orderMutex.Unlock()
+}
